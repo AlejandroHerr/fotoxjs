25862a23d0d0a50d612849aeaf102bf1bf4aac82
AlejandroHerr <alejandrohnc88@gmail.com>
2017-04-16 01:40:18 +0200

    Format

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import Root from './js/Root';
 
-render(<Root />,
-  document.getElementById('root'),
-);
+render(<Root />, document.getElementById('root'));
