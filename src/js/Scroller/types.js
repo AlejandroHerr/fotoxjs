6b093e88a8d8821e34c6f396eba3118f2c336b24
AlejandroHerr <alejandrohnc88@gmail.com>
2017-04-16 22:05:17 +0200

    Fix quotes

--- src/js/Scroller/types.js
+++ src/js/Scroller/types.js
@@ -1,8 +1,8 @@
 export type Dimensions = {
-  "width": number,
-  "height": number,
-  "top": number,
-  "right": number,
-  "bottom": number,
-  "left": number,
+  width: number,
+  height: number,
+  top: number,
+  right: number,
+  bottom: number,
+  left: number,
 };
