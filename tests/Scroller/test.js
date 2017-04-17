aa5337e4caf1f941aa036b31b2c93721813c4c93
AlejandroHerr <alejandrohnc88@gmail.com>
2017-04-18 00:02:41 +0200

    No scroller test

--- tests/Scroller/test.js
+++ tests/Scroller/test.js
@@ -1,12 +1,10 @@
 import test from 'tape';
 import scrollAreaSpec from './ScrollArea_spec';
 import scrollSpec from './Scroll_spec';
-import scrollerSpec from './Scroller_spec';
 import actionsSpec from './actions_spec';
 
 test('+ Scroller', ({ test: subtest }) => {
   subtest('-> ScrollArea', scrollAreaSpec);
   subtest('-> Scroll', scrollSpec);
-  subtest('-> Scroller', scrollerSpec);
   subtest('-> actions', actionsSpec);
 });
